46cbbc65d04e0f15d566e9654060eadcd2277109
easkl <easkl@example.com>
2025-11-29 01:42:12 +0900

    navigation.py 완성, 추후 수정 필요할수도

--- agent/pe_e1_i1/pe_e1_i1_agent.py
+++ agent/pe_e1_i1/pe_e1_i1_agent.py
@@ -1,6 +1,7 @@
 from kspdg.agent_api.base_agent import KSPDGBaseAgent
 from kspdg.pe1.e1_envs import PE1_E1_I1_Env
 from kspdg.agent_api.runner import AgentEnvRunner
+from observation_log import observation_history
 import time
 
 class PulsedAgent(KSPDGBaseAgent):
@@ -9,20 +10,12 @@ class PulsedAgent(KSPDGBaseAgent):
         self.is_on = False
 
     def get_action(self, observation):
-        time.sleep(1.0)
-        self.is_on = not self.is_on
-
-        if self.is_on:
-            return {
-                "burn_vec": [0, 1.0, 0, 0.3],
-                "ref_frame": 0
-            }
-        else:
-            return {
-                "burn_vec": [0, 0.0, 0, 0.0],
-                "ref_frame": 0
-            }
-
+        observation_history.append(observation)
+        return {
+            "burn_vec": [0, 1, 0, 1],
+            "ref_frame": 0
+        }
+        
 if __name__ == "__main__":
     agent = PulsedAgent()
     runner = AgentEnvRunner(
---
+++ agent/pe_e1_i1/propulsion.py
@@ -0,0 +1,4 @@
+'''이 파일에서 해야 하는것 navigation.py의 navigation함수에서 반환한 가속도 벡터를
+받아서 rcs의 분사 벡터로 전환해서 크기 3의 array를 반환해야 한다'''
+from numpy import np
+from navigation import navigation
